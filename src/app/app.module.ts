4617f9ab780b707173345982b1bb6532677c1b2c
DarshanaJK <128844702+DarshanaJK@users.noreply.github.com>
2023-12-19 11:41:51 +0530

    create login page 8& API call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -27,7 +27,7 @@ import {HttpClientModule} from "@angular/common/http";
     HttpClientModule,
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
   ],
   bootstrap: [AppComponent]
 })
